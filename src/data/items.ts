cc02cc3eafc787ee0b1eb2e14df608d1c8d9dd11
pvcapuano <pvcapuano@gmail.com>
2021-10-26 18:32:39 -0200

    create data e types

---
+++ src/data/items.ts
@@ -0,0 +1,9 @@
+import { Item } from '../types/Item'
+
+export const items: Item[] = [
+    { date: new Date(2021, 9, 15), category: 'food', title: 'Pizza', value: 39.10 },
+    { date: new Date(2021, 9, 15), category: 'food', title: 'Jantar', value: 39.10 },
+    { date: new Date(2021, 9, 16), category: 'rent', title: 'Aluguel', value: 39.10 },
+    { date: new Date(2021, 10, 18), category: 'salary', title: 'Salário', value: 4000 }
+
+]
