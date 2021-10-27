9ee576101283f811c3c45e698aae5bd0bb15cd85
pvcapuano <pvcapuano@gmail.com>
2021-10-27 19:17:31 -0200

    table area component

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,29 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import * as C from "./App.styles";
 import { Item } from "./types/Item";
 import { Category } from "./types/Category";
 import { categories } from "./data/categories";
 import { items } from "./data/items";
-import { getCurrentMonth } from "./helpers/dateFilter";
+import { getCurrentMonth, filterListByMonth } from "./helpers/dateFilter";
+import { TableArea } from "./components/TableArea";
 
 const App = () => {
   const [list, setList] = useState(items);
   const [filteredList, setFilteredList] = useState<Item[]>([]);
   const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
+
+  useEffect(() => {
+    setFilteredList(filterListByMonth(list, currentMonth));
+  }, [list, currentMonth]);
   return (
     <C.Container>
       <C.Header>
         <C.HeaderText>Sistema Financeiro</C.HeaderText>
       </C.Header>
-      <C.Body>...</C.Body>
+      <C.Body>
+        <TableArea />
+      </C.Body>
     </C.Container>
   );
 };
