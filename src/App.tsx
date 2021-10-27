84cbea9175ab5a16efceb94975c06500febd2478
pvcapuano <pvcapuano@gmail.com>
2021-10-27 18:45:29 -0200

    create a dateFilter component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { useState } from "react";
 import * as C from "./App.styles";
+import { Item } from "./types/Item";
+import { Category } from "./types/Category";
+import { categories } from "./data/categories";
+import { items } from "./data/items";
+import { getCurrentMonth } from "./helpers/dateFilter";
 
 const App = () => {
+  const [list, setList] = useState(items);
+  const [filteredList, setFilteredList] = useState<Item[]>([]);
+  const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
   return (
     <C.Container>
       <C.Header>
